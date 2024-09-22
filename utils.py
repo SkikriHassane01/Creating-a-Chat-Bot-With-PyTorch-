374ab38d97bd8ec38d6d5f5ca729553e5baf6c66
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-22 15:45:12 +0100

    complete the utils file

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 import nltk
 # nltk.download('punkt_tab')
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 stemmer = PorterStemmer()
 
 # =============tokenization function======================
@@ -13,15 +14,18 @@ def stem(word):
     return stemmer.stem(word, to_lowercase=True)
 
 # ===============bag of words function====================
-def bag_of_words(tokenized_sentence, all_words):
-    pass
-
-# testing 
-string = "How long this project will tack"
-print (f"string: {string}")
-print (f"tokenized string: {tokenize(string)}")
-
-words = ['organize', "organizes", "organizing"]
-print (f"words: {words}")
-stemmed_words = [stem(w) for w in words]
-print (f"steaming result: {stemmed_words}")
+def bag_of_words(pattern_sentence, all_words):
+    """
+    1 for each known word that exists in the sentence, 0 otherwise
+    sentence = ["hello", "how", "are", "you"]
+    all_words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    
+    pattern_sentence = [stem(word) for word in pattern_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    
+    for idx, word in enumerate(all_words):
+        if word in pattern_sentence:
+            bag[idx] = 1.0
+    return bag
