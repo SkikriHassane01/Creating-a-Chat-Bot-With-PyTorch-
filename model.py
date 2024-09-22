94bffbbe87a57363d8207519df0439e2d07ac879
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-22 19:12:51 +0100

    2.update the model to initializes and loads the Hugging Face

--- model.py
+++ model.py
@@ -1,19 +1,16 @@
+"""
+    initializes and loads the Hugging Face model (DistilBERT) for text classification.
+"""
+
 import torch
-import torch.nn as nn
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size)
-        self.l2 = nn.Linear(hidden_size, hidden_size)
-        self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
+def load_model(num_labels):
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels)
     
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        out = self.relu(out)
-        out = self.l3(out)
-        return out
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    
+    return tokenizer, model, device
